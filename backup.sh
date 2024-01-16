9a5c7b44e41dca23ae17cccd2aaf45179a6ac124
Anton Legonechko <103181968+LegitOneTwo@users.noreply.github.com>
2024-01-16 15:09:58 +1100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -5,6 +5,7 @@ echo "Backing up files..." #send message about starting the process
 backup_dir="/etc/network/interfaces /etc/dhcp/dhcpd.conf /etc/passwd /etc/group" #files to backup
 dest_dir="/archive" #Destination of the backup archive
 
+mkdir -p /archive
 zip $dest_dir/$(hostname -s)-$(date +%d.%m.%y).zip $backup_dir #make an archive with date in the name using zip command
 
 echo "Backup process done" #send message to user about success
