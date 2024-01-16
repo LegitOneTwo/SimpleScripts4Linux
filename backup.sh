3226be81f37f75fa51c0e0e2c45ce33af2451497
Anton Legonechko <103181968+LegitOneTwo@users.noreply.github.com>
2024-01-16 14:57:28 +1100

    Add files via upload
    Скрипт для создания резервных копий

---
+++ backup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Backing up files..." #send message about starting the process
+
+backup_dir="/etc/network/interfaces /etc/dhcp/dhcpd.conf /etc/passwd /etc/group" #files to backup
+dest_dir="/archive" #Destination of the backup archive
+
+zip $dest_dir/$(hostname -s)-$(date +%d.%m.%y).zip $backup_dir #make an archive with date in the name using zip command
+
+echo "Backup process done" #send message to user about success
